28ee538304e339ad17cd06a4661acb4df03a3bcd
Emre Akyüz <aemreakyuz@gmail.com>
2023-10-27 00:31:56 +0300

    Row completed

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
 import instance from "./utils/axios.js";
+import "./Row.css";
+
+const base_image_url = "https://image.tmdb.org/t/p/original";
+const API_KEY = "2d556cfd56214c861b900b221a8b327d";
 
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
@@ -15,12 +19,23 @@ function Row({ title, fetchUrl }) {
     fetchData();
   }, [fetchUrl]);
 
-  console.log("movies", movies);
-
+  // console.log("movies", movies);
+  //const poster_paths = movies.map((movie) => movie.poster_path);
+  //console.log(poster_paths);
   return (
-    <div className="row_container">
+    <div className="row">
       <h1>{title}</h1>
-      <div className="posters">posters</div>
+
+      <div className="row__posters flex p-4 bg-black  ">
+        {movies.map((movie) => (
+          <img
+            className="row__poster"
+            key={movie.id}
+            src={`${base_image_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
   );
 }
